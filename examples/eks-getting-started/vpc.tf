39a42b8afe8eb6a5a1ac6a9e4b0262eb058c7833
sriison <40303851+sriison@users.noreply.github.com>
2019-10-23 18:54:13 +0530

    Update vpc.tf

--- examples/eks-getting-started/vpc.tf
+++ examples/eks-getting-started/vpc.tf
@@ -8,6 +8,8 @@
 
 resource "aws_vpc" "demo" {
   cidr_block = "10.0.0.0/16"
+  enable_dns_support   = true
+  enable_dns_hostnames = true
   
  # Used for the internal VPC DNS resolution
   enable_dns_support   = true
